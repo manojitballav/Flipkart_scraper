1e86f64207dc5774f7ac38c81e8fb3084d68497b
manojitballav <manojitocto@gmail.com>
2019-06-11 10:04:40 +0530

    Code to get reviews added, still lacking support on READ MORE option

---
+++ master.py
@@ -0,0 +1,64 @@
+# This program is the primary control program to scrap reviews from Flipkart
+from pymongo import MongoClient
+import re,time
+from selenium import webdriver
+from selenium import *
+
+# connection to the db
+client = MongoClient('10.56.133.21',27017)
+db = client['flipkart']
+col1 = db['r_data']
+# webdrive connection
+driver = webdriver.Chrome()
+
+def r_update(rating,heading,body,pc):
+    col2 = db[pc]
+    col2.update_one({"body": body},{'$set':{"body":body,"rating":rating,"heading":heading}},upsert=True)
+
+def sreview(r_link,pn,pc):
+    for val in range(1,int(pn)+1):
+        driver.get(r_link+str(val))
+        for kal in range(3,13):
+            kal = str(kal)
+            try:
+                rating = driver.find_element_by_xpath('/html/body/div[1]/div/div[3]/div[2]/div/div[2]/div['+kal+']/div/div/div/div[1]/div').text
+            except Exception as e:
+                print(e)
+            try:
+                heading = driver.find_element_by_xpath('/html/body/div[1]/div/div[3]/div[2]/div/div[2]/div['+kal+']/div/div/div/div[1]/p').text
+            except Exception as e:
+                print(e)
+            try:
+                body = driver.find_element_by_xpath('/html/body/div[1]/div/div[3]/div[2]/div/div[2]/div['+kal+']/div/div/div/div[2]/div/div/div').text
+            except Exception as e:
+                print(e)
+            r_update(rating,heading,body,pc)
+            time.sleep(4)
+    driver.quit()
+
+def read():
+    for dic in col1.find({}):
+        link = dic['link']
+        r_link = dic['r_link']
+        pc = dic['db']
+        driver.get(link)
+        try:
+            review = 0
+            if(review == 0):
+                review = (driver.find_element_by_xpath('/html/body/div[1]/div/div[3]/div[2]/div[1]/div[2]/div[2]/div/div[2]/div/div/span[2]/span/span[3]').text)
+                review = review.replace(' Reviews','')
+                review = review.replace(' ','')
+                review = review.replace(',','')
+            else:
+                review = (driver.find_element_by_xpath('/html/body/div[1]/div/div[3]/div[2]/div[1]/div[2]/div[2]/div/div[2]/div/div/span[2]/span/span[3]').text)
+        except Exception as NoSuchElementException:
+            pass
+            print("No element found")
+        pn = (int(review)//10)
+        sreview(r_link,pn,pc)
+
+def main():
+    read()
+
+if __name__ == '__main__':
+    main()
