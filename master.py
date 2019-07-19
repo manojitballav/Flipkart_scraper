86cbd03b5501b5ad7596f5b2ba450f0545524286
manojitballav <manojitocto@gmail.com>
2019-07-19 18:46:49 +0530

    Fixed local variable issue

--- master.py
+++ master.py
@@ -9,13 +9,17 @@ client = MongoClient('10.56.133.21',27017)
 db = client['flipkart']
 col1 = db['r_data']
 # webdrive connection
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 
 def r_update(rating,heading,body,pc):
     col2 = db[pc]
     col2.update_one({"body": body},{'$set':{"body":body,"rating":rating,"heading":heading}},upsert=True)
 
 def sreview(r_link,pn,pc):
+    read = 0
+    rating = 0
+    heading = 0
+    body = 0
     for val in range(1,int(pn)+1):
         driver.get(r_link+str(val))
         for kal in range(3,13):
@@ -37,7 +41,7 @@ def sreview(r_link,pn,pc):
             except Exception as e:
                 print(e)
             r_update(rating,heading,body,pc)
-            time.sleep(4)
+            time.sleep(1)
     driver.quit()
 
 def read():
