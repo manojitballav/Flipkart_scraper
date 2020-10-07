d5da4820b5bc30e636b425f0ab91622c39d37bbc
manojitballav <manojitocto@gmail.com>
2020-10-07 21:31:18 +0530

    Added date functionality

--- master.py
+++ master.py
@@ -3,6 +3,8 @@ from pymongo import MongoClient
 import re,time
 from selenium import webdriver
 from selenium import *
+from datetime import datetime, timedelta
+from datetime import date
 
 # connection to the db
 client = MongoClient('10.56.146.102',27017)
@@ -11,15 +13,16 @@ col1 = db['r_data']
 # webdriver connection
 driver = webdriver.Chrome()
 
-def r_update(rating,heading,body,pc):
+def r_update(rating,heading,body,date,pc):
     col2 = db[pc]
-    col2.update_one({"body": body},{'$set':{"body":body,"rating":rating,"heading":heading}},upsert=True)
+    col2.update_one({"body": body},{'$set':{"body":body,"rating":rating,"heading":heading,"date":date}},upsert=True)
 
 def sreview(r_link,pn,pc):
     read = 0
     rating = 0
     heading = 0
     body = 0
+    date = 0
     # griver = webdriver.Firefox()f
     for val in range(1,int(pn)+1):
         driver.get(r_link+str(val))
@@ -44,17 +47,38 @@ def sreview(r_link,pn,pc):
                 print(body)
             except Exception as e:
                 print(e)
-            r_update(rating,heading,body,pc)
+            try:
+                # when character does not shift
+                date = driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div[2]/div['+kal+']/div/div/div/div[3]/div[1]/p[3]').text
+            except:
+                # when character shifts
+                date = driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div[2]/div['+kal+']/div/div/div/div[4]/div[1]/p[3]').text
+            # to determine if integer is present in date string
+            if (bool(re.search(r'\d', date)) == True):
+                # get only integer from string
+                tmp5 = re.findall(r'\d+',date)
+                res5 = list(map(int,tmp5))
+                date = int(res5[0])
+                # get actual date using time delta
+                n = int(date)
+                date = datetime.now()-timedelta(days=n)
+                date = (date.strftime("%d"+"/"+"%m"+"/"+"%Y"))
+            else:
+                # convert today into date
+                x = datetime.now()
+                date = (x.strftime("%d"+"/"+"%m"+"/"+"%Y"))
+            print(date)
+            r_update(rating,heading,body,date,pc)
             # time.sleep(5)
     driver.quit()
 
 def read():
-    for dic in col1.find({'db':{"$in":["MOBFVEATBBRGJBKH"]}}):
+    for dic in col1.find({'db':{"$in":["MOBFVQJ5TZTF5JHC"]}}):
         link = dic['link']
         r_link = dic['r_link']
         pc = dic['db']
         driver.get(link)
-        review = int(1916)
+        review = int(317)
         # try:
         #     review = 0
         #     if(review == 0):
